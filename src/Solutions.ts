32c7691f6fbb38b98599fe876f57ade162526ffb
deadmanShadow <raihanshamil33@gmail.com>
2025-05-08 23:11:40 +0600

    changes for using multiple times of intarface

--- src/Solutions.ts
+++ src/Solutions.ts
@@ -72,11 +72,6 @@
     price: number;
   }
 
-  interface Product {
-    name: string;
-    price: number;
-  }
-
   function getMostExpensiveProduct(products: Product[]): Product | null {
     if (products.length === 0) {
       return null;
